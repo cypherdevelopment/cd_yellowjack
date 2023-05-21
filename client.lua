c336f505a5c4f4280e1d7a4da520a3da36fbd161
Kayne <78748546+DevKayne@users.noreply.github.com>
2023-05-21 13:40:34 +0800

    nui event and base function

--- client.lua
+++ client.lua
@@ -1,12 +1,36 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local uiopen = false 
 local clockedOn = false 
+local target = exports['qb-target']
 
 -- Is Player Clocked On? --
+CreateThread(function()
+    while true do 
 
+        -- Wait 30 Seconds --
+        Wait(30000)
+
+        -- Get Player and Player Job Status --
+end)
 
 -- Bar Fridge -- 
+-- Targets -- 
 
 -- Food Section -- 
+-- Targets
 
 -- UI (Invoicing) Section --
+RegisterNetEvent('cd_yellowjack:useui', function()
+    if not uiopen then 
+        SendNUIMessage({
+            type = "openui",
+        })
+        uiopen = true 
+        SetNuiFocus(true, true)
+    else 
+        SendNUIMessage({
+            type = "closeui",
+        })
+        uiopen = false
+        SetNuiFocus(false, false)
+end)
