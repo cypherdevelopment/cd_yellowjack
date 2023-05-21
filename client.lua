19f282a90adcd655a758459fc26892e33a9400ba
Kayne <78748546+DevKayne@users.noreply.github.com>
2023-05-21 12:23:55 +0800

    init

---
+++ client.lua
@@ -0,0 +1,12 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local uiopen = false 
+local clockedOn = false 
+
+-- Is Player Clocked On? --
+
+
+-- Bar Fridge -- 
+
+-- Food Section -- 
+
+-- UI (Invoicing) Section --
