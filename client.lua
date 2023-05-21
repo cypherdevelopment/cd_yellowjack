4828216fb0a8c07f3ef4cd27f733f225b6e3fa21
DevKayne <78748546+DevKayne@users.noreply.github.com>
2023-05-21 17:31:53 +0800

    push: basic ui, clear peds, duty toggle

--- client.lua
+++ client.lua
@@ -1,16 +1,65 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local uiopen = false 
-local clockedOn = false 
 local target = exports['qb-target']
 
--- Is Player Clocked On? --
+-- Check Duty Status every 30 seconds --
 CreateThread(function()
     while true do 
 
         -- Wait 30 Seconds --
-        Wait(30000)
+        Wait(10000)
 
         -- Get Player and Player Job Status --
+        local Player = QBCore.Functions.GetPlayerData()
+        DutyStatus = Player.job.onduty
+    end
+end)
+
+-- Draw Blip -- 
+local Location = vector3(1986.38, 3049.54, 47.22)
+local Blip = AddBlipForCoord(Location)
+SetBlipSprite(Blip, 93)
+BeginTextCommandSetBlipName('Bar')
+
+-- Clock On/Off -- 
+RegisterNetEvent('cd_yellowjack:dutytoggle', function()
+    if not DutyStatus then 
+        TriggerServerEvent('QBCore:ToggleDuty')
+    else 
+        TriggerServerEvent('QBCore:ToggleDuty')
+    end 
+end)
+
+target:AddBoxZone("dutytoggle", vector3(1981.39, 3051.11, 47.21), 1.5, 1.6, {
+    name = "dutytoggle",
+    heading = 151.81,
+    debugPoly = false, 
+    minZ = 47.00,
+    maxZ = 48.00,
+}, {
+    options = {
+        {
+            type = "client",
+            event = "cd_yellowjack:dutytoggle",
+            icon = "fas fa-sign-in alt",
+            label = "Clock On/Off",
+            job = 'yellowjack',
+        }
+    },
+    distance = 1.0,
+})
+
+-- Clear All Peds -- 
+CreateThread(function()
+    while true do
+        
+        -- Wait 30 Seconds -- 
+        Wait(30000)
+
+        -- Clear Peds -- 
+        ClearAreaOfPeds(Location.x, Location.y, Location.z, 1000.0, 1)
+
+    end 
 end)
 
 -- Bar Fridge -- 
@@ -21,16 +70,41 @@ end)
 
 -- UI (Invoicing) Section --
 RegisterNetEvent('cd_yellowjack:useui', function()
-    if not uiopen then 
+    if not uiopen then
         SendNUIMessage({
-            type = "openui",
+            type = "openui"
         })
-        uiopen = true 
+        uiopen = true
         SetNuiFocus(true, true)
-    else 
+    else
         SendNUIMessage({
-            type = "closeui",
+            type = "closeui"
         })
         uiopen = false
-        SetNuiFocus(false, false)
+    end
 end)
+
+RegisterNuiCallback('cancelbtn', function(_, cb)
+    cb({})
+
+    SetNuiFocus(false, false)
+end)
+
+target:AddBoxZone('ui', vector3(1982.32, 3053.33, 47.22), 1.5, 1.6, {
+    name = "ui",
+    heading = 62.94,
+    debugPoly = false, 
+    minZ = 47.00,
+    maxZ = 48.00,
+}, {
+    options = {
+        {
+            type = "client",
+            event = "cd_yellowjack:useui",
+            icon = "fas fa-money-bill",
+            label = "Set Invoice",
+            --job = "yellowjack",
+        }
+    },
+    distance = 1.0,
+})
---
+++ html/index.js
@@ -0,0 +1,26 @@
+$(function() {
+    // Hide Div //
+    $('.container').hide();
+
+    window.addEventListener("message", function(event) {
+        let data = event.data; 
+
+        if (data.type === "openui") {
+            $('.container').show();
+        }
+
+        if (data.type === "closeui") {
+            $(".container").hide();
+        }
+
+    })
+})
+
+document.getElementById('closebtn').addEventListener('click', () => {
+    // Hide Div // 
+    $('.container').hide();
+
+    // Release Focus //
+    axios.post(`https://${GetParentResourceName()}/cancelbtn`, {});
+
+})
