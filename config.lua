ce1ba95b21da8b8d1fdaa2c9edbf1092b8e45c2a
DevKayne <78748546+DevKayne@users.noreply.github.com>
2023-05-22 21:13:55 +0800

    push: config file
    configuration file for easier access.

--- config.lua
+++ config.lua
@@ -1,5 +1,16 @@
 -- Configuration - -
-Config = {}
+Config = {
 
--- Webhook URL -- 
-Config.WebhookURL = "https://discord.com/api/webhooks/1109976260219768964/gvo2OWSj37v40ty21MyLyZlrL7SPk0SEe6Z6qBRLmSJ2Duc_EL7mm0scp5Bf0rxIH-e7"
+    -- Item Prices --
+    ItemPrices = {
+        beer = 200,
+        whiskey = 350, 
+        vodka = 300,
+        wine = 250, 
+        water = 100, 
+        coffee = 150
+    },
+    
+    -- Webhook URL -- 
+    WebhookURL = "https://discord.com/api/webhooks/1109976260219768964/gvo2OWSj37v40ty21MyLyZlrL7SPk0SEe6Z6qBRLmSJ2Duc_EL7mm0scp5Bf0rxIH-e7",
+}
