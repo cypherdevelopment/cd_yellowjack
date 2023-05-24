8a4049b6237d37a9ddd1ddfaf0a707f153dc4630
aw2884463 <awroblos1@student.rccd.edu>
2023-05-23 17:59:14 -0700

    Fixed X Button

--- client.lua
+++ client.lua
@@ -1,6 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local uiopen = false 
+local uiopen = false
+local billingopen = false 
 local target = exports['qb-target']
+local Location = vector3(1986.38, 3049.54, 47.22)
 
 -- Check Duty Status every 30 seconds --
 CreateThread(function()
@@ -15,19 +17,24 @@ CreateThread(function()
     end
 end)
 
+AddEventHandler('onResourceStart', function()
+
+    DrawBlip()
+
+end)
+
 -- Draw Blip -- 
-local Location = vector3(1986.38, 3049.54, 47.22)
-local Blip = AddBlipForCoord(Location)
-SetBlipSprite(Blip, 93)
-BeginTextCommandSetBlipName('Bar')
+function DrawBlip()
+    local Blip = AddBlipForCoord(Location)
+    SetBlipSprite(Blip, 93)
+    BeginTextCommandSetBlipName('Bar')
+end
+
 
 -- Clock On/Off -- 
-RegisterNetEvent('cd_yellowjack:dutytoggle', function()
-    if not DutyStatus then 
-        TriggerServerEvent('QBCore:ToggleDuty')
-    else 
-        TriggerServerEvent('QBCore:ToggleDuty')
-    end 
+RegisterNetEvent('cd_yellowjack:dutytoggle')
+AddEventHandler('cd_yellowjack:dutytoggle', function()
+    TriggerServerEvent('QBCore:ToggleDuty')
 end)
 
 target:AddBoxZone("dutytoggle", vector3(1981.39, 3051.11, 47.21), 1.5, 1.6, {
@@ -65,32 +72,153 @@ end)
 -- Bar Fridge -- 
 -- Targets -- 
 --vector4(1981.75, 3052.28, 47.22, 55.32)--
+target:AddBoxZone("barfridge", vector3(1981.75, 3052.28, 47.22), 1.5, 1.6, {
+    name = "barfridge",
+    heading = 55.32,
+    debugPoly = false,
+    minZ = 47.00,
+    maxZ = 48.00,
+}, {
+    options = {
+        {
+            icon = "fa-solid fa-beer",
+            label = "Alcohol: Beer",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'beer')
+                QBCore.Functions.Notify('You grabbed a Beer!', primary, 5000)
+            end 
+        },
+        {
+            icon = "fa-solid fa-whiskey-glass",
+            label = "Alcohol: Whiskey",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'whiskey')
+                QBCore.Functions.Notify('You grabbed a Whiskey!', primary, 5000)
+            end 
+        },
+        {
+            icon = "fa-solid fa-martini-glass-citrus",
+            label = "Alcohol: Vodka",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'vodka')
+                QBCore.Functions.Notify('You grabbed a Vodka!', primary, 5000)
+            end 
+        },
+        {
+            icon = "fa-solid fa-wine-glass",
+            label = "Alcohol: Wine",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'wine')
+                QBCore.Functions.Notify('You grabbed a Wine!', primary, 5000)
+            end 
+        },
+        {
+            icon = "fa-solid fa-bottle-water",
+            label = "Drink: Water",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'water_bottle')
+                QBCore.Functions.Notify('You grabbed a Water!', primary, 5000)
+            end 
+        },
+        {
+            icon = "fa-solid fa-mug-hot",
+            label = "Drink: Coffee",
+            job = "yellowjack",
+            action = function()
+                TriggerServerEvent('cd_yellowjack:GiveItem', 'coffee')
+                QBCore.Functions.Notify('You grabbed a Coffee!', primary, 5000)
+            end 
+        },
+    },
+    distance = 1.0,
+})
 
 -- Food Section -- 
 -- Targets --
 --vector4(1984.18, 3049.84, 47.22, 323.95)--
 
 -- UI (Invoicing) Section --
-RegisterNetEvent('cd_yellowjack:useui', function()
-    if not uiopen then
+RegisterNetEvent('cd_yellowjack:useui')
+AddEventHandler('cd_yellowjack:useui', function()
+        if not uiopen then
+            SendNUIMessage({
+                type = "openui"
+            })
+            uiopen = true
+            SetNuiFocus(true, true)
+        else
+            SendNUIMessage({
+                type = "closeui"
+            })
+            uiopen = false
+        end
+end)
+
+RegisterNuiCallback('sbmtbtn', function(data, cb)
+    name = data.name
+    id = data.id
+    amount = data.amount
+    desc = data.desc
+    print(name,id,amount,desc)
+    AddBill(id,amount,desc)
+    SendWebHook(name,amount,desc)
+    uiopen = false
+    cb({})
+    SetNuiFocus(false, false)
+end)
+RegisterNuiCallback('closeui', function(data, cb)
+    billingopen = false
+    cb({})
+    SetNuiFocus(false, false)
+end)
+
+function AddBill(id,amount,desc) 
+    TriggerServerEvent('cd_yellowjack:AddBill',id,amount,desc)
+    Citizen.Wait(50)
+end
+
+function RemoveBill(id,amount,desc) 
+    TriggerServerEvent('cd_yellowjack:RemoveBill',id,amount,desc)
+end
+
+function GetTab()
+    local Player = QBCore.Functions.GetPlayerData()
+    QBCore.Functions.TriggerCallback('cd_yellowjack:GetTab', function(cb)
+        if cb == nil then 
+            print('here')
+        end
+            print(cb.citizenid,cb.amount)
+    end,Player.citizenid)
+end
+
+-- Webhook System --
+function SendWebHook(name, amount, desc)
+    TriggerServerEvent('cd_yellowjack:SendWebHook',name,amount,desc)
+end 
+
+
+
+RegisterCommand('OpenBarTab',function()
+    -- GetTab()
+    if not billingopen then
         SendNUIMessage({
-            type = "openui"
+            type = "openbilling"
         })
-        uiopen = true
+        billingopen = true
         SetNuiFocus(true, true)
     else
         SendNUIMessage({
-            type = "closeui"
+            type = "closebilling"
         })
-        uiopen = false
+        billingopen = false
+        SetNuiFocus(false, false)
     end
-end)
-
-RegisterNuiCallback('cancelbtn', function(_, cb)
-    cb({})
-
-    SetNuiFocus(false, false)
-end)
+end,false)
 
 target:AddBoxZone('ui', vector3(1982.32, 3053.33, 47.22), 1.5, 1.6, {
     name = "ui",
@@ -105,7 +233,7 @@ target:AddBoxZone('ui', vector3(1982.32, 3053.33, 47.22), 1.5, 1.6, {
             event = "cd_yellowjack:useui",
             icon = "fas fa-money-bill",
             label = "Set Invoice",
-            --job = "yellowjack",
+            job = "yellowjack",
         }
     },
     distance = 1.0,
--- server.lua
+++ server.lua
@@ -1,1 +1,79 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+
+AddEventHandler('onResourceStart', function(resourceName)
+    QBCore.Functions.AddJob('yellowjack', {
+        label = 'YellowJack',
+        defaultDuty = true,
+        offDutyPay = false,
+        grades = {
+            ['0'] = {
+                name = 'Freelancer',
+                payment = 10
+            }
+        }
+    })
+end)
+
+RegisterServerEvent('cd_yellowjack:AddBill')
+AddEventHandler('cd_yellowjack:AddBill', function(id,amount,desc)
+    MySQL.insert(
+        'INSERT INTO barbills (citizenid, amount, description) VALUES (@name, @amount, @desc);',
+        {
+            ['@name'] = id,
+            ['@amount'] = amount,
+            ['@desc'] = desc
+        },
+        function(result)
+    end)
+end)
+
+RegisterServerEvent('cd_yellowjack:RemoveBill')
+AddEventHandler('cd_yellowjack:RemoveBill', function(id,amount,desc)
+    MySQL.Query(
+        'DELETE FROM `barbills` WHERE @name,@amount,@desc',
+        {
+            ['@name'] = id,
+            ['@amount'] = amount,
+            ['@desc'] = desc
+        },
+        function(result)
+    end)
+end)
+
+
+QBCore.Functions.CreateCallback('cd_yellowjack:GetTab', function(source, cb, id)
+
+    MySQL.Async.fetchAll(
+        'SELECT * FROM barbills WHERE citizenid = @citizenid',
+        {
+            ['@citizenid'] = id
+        },
+        function(result)
+            for k,v in ipairs(result) do 
+                print(v.amount)
+            end
+            if result == nil then 
+                print('nope')
+            end
+            
+            cb(result)
+        end)
+end)
+
+
+RegisterServerEvent('cd_yellowjack:SendWebHook')
+AddEventHandler('cd_yellowjack:SendWebHook', function(name, amount, desc)
+    local embeds = {
+        {
+            ["title"] = "New Invoice!",
+            ["description"] = "**Customer**: " .. name .. "\n**Amount**: " .. amount .. "\n**Description**: " .. desc,
+            ["type"] = "rich",
+            ["color"] = 16776960,
+            ["footer"] = {
+                ["text"] = "YellowJack Bar"
+            },
+        }
+    }
+
+    PerformHttpRequest(Config.WebhookURL, function(err, text, headers) end, 'POST', json.encode({username = "YellowJack Invoice System", embeds = embeds}), { ['Content-Type'] = 'application/json'})
+end)
