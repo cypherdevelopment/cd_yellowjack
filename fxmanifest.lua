28a487d47d72750e5d9c246e98afb8c87d49c85a
aw2884463 <awroblos1@student.rccd.edu>
2023-05-23 17:52:54 -0700

    Merged billing with yellowjack

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,16 @@ fx_version 'cerulean'
 game 'gta5'
 
 client_script 'client.lua'
+server_script '@oxmysql/lib/MySQL.lua'
 server_script 'server.lua'
+shared_script 'config.lua'
 
 ui_page 'html/index.html'
 
 files {
     'html/index.html',
     'html/index.js',
+    'html/billing.js',
     'html/style.css',
     'html/yj.jpg'
 }
--- html/index.js
+++ html/index.js
@@ -1,6 +1,7 @@
 $(function() {
     // Hide Div //
-    $('.container').hide();
+$('.container').hide();
+$('.billing').hide();
 
     window.addEventListener("message", function(event) {
         let data = event.data; 
@@ -12,15 +13,27 @@ $(function() {
         if (data.type === "closeui") {
             $(".container").hide();
         }
+        if (data.type === "openbilling") {
+            $(".billing").show();
+        }
+        if (data.type === "closebilling") {
+            $(".billing").hide();
+        }
 
     })
 })
 
 document.getElementById('submitbtn').addEventListener('click', () => {
+
+    let name = document.getElementById('name').value
+    let id = document.getElementById('id').value
+    let amount = document.getElementById('amount').value
+    let desc = document.getElementById('desc').value
+
     // Hide Div // 
     $('.container').hide();
 
     // Release Focus //
-    axios.post(`https://${GetParentResourceName()}/cancelbtn`, {});
+    axios.post(`https://${GetParentResourceName()}/sbmtbtn`, {name,id,amount,desc});
 
 })
