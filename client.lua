dad61e6c53e92a45915ab5a7f6cac55eac44b3eb
DevKayne <78748546+DevKayne@users.noreply.github.com>
2023-05-25 19:30:59 +0800

    qol update

--- client.lua
+++ client.lua
@@ -140,9 +140,8 @@ RegisterNuiCallback('sbmtbtn', function(data, cb)
     local id = data.id
     local amount = data.amount
     local desc = data.desc
-    print(name,id,amount,desc)
     AddBill(id,amount,desc)
-    --SendWebHook(name,amount,desc)
+    SendWebHook(name,amount,desc)
     uiopen = false
     cb({})
     SetNuiFocus(false, false)
